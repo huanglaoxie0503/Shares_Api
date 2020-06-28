4e2d657938a46267a194a41051202410f873380f
e <2251018029@qq.com>
2020-06-28 13:34:42 +0800

    添加 app 并新建 model

--- Shares_Api/settings.py
+++ Shares_Api/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.users',
+    'apps.stock',
+    'apps.factor',
+    'apps.operation',
 ]
 
 AUTH_USER_MODEL = "users.UserProfile"
--- apps/users/models.py
+++ apps/users/models.py
@@ -22,3 +22,17 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    """
+    验证码
+    """
+    code = models.CharField(max_length=20, verbose_name='验证码')
+    email = models.EmailField(max_length=50, verbose_name='邮箱')
+    send_type = models.CharField(max_length=10, choices=(('register', '注册'), ('forget', '找回密码')))
+    send_time = models.DateTimeField(verbose_name='发送时间', auto_now_add=True)
+
+    class Meta:
+        verbose_name = '邮箱验证码'
+        verbose_name_plural = verbose_name
